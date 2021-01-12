0c46fc71322a611331a24be204b765368716e11f
DESKTOP-F1G7JDJ\Sebastian Pernia <sebaspernia@gmail.com>
2021-01-12 13:14:46 +0100

    Subida del proyecto

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+
+const indexRoutes = require('./routes/index')
+
+const userRoutes = require('./routes/user')
+
+const productRoutes = require('./routes/product')
+
+const loginRoutes = require('./routes/login')
+
+
+app.use('/', indexRoutes)
+
+app.use('/user', userRoutes)
+
+app.use('/product', productRoutes)
+
+app.use('/login', loginRoutes)
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
